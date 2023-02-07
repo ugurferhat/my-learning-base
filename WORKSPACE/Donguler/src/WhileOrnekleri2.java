266967efc36e15ef0fa5dfe6cb9253868f0e721c
ugurferhat <124739305+ugurferhat@users.noreply.github.com>
2023-02-08 01:29:24 +0300

    Update WhileOrnekleri2.java

--- WORKSPACE/Donguler/src/WhileOrnekleri2.java
+++ WORKSPACE/Donguler/src/WhileOrnekleri2.java
@@ -3,7 +3,19 @@ public class WhileOrnekleri2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int star = scan.nextInt();
+		int i =1;
+		while(i<=star) {
+		int k =1;
+			while(k<=i){
+			System.out.print("*");
+			k++;
+			}
+			System.out.println();
+			i++;
+		}
+		
 	}
 
 }
